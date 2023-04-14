96b6158da5d837ca7a787c7f5c5c5fd52024a0d2
Mr Hee <matteo.hee@spschools.org>
2023-04-14 09:03:44 -0600

    Initial

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        colour newColour = new colour("purple", 200, 5, 200);
+
+
+        System.out.println(newColour.getHexCode());
+    }
+}
---
+++ src/colour.java
@@ -0,0 +1,65 @@
+public class colour {
+
+    int r; //how much red 0-255
+    int g;
+    int b;
+
+    String commonName;
+
+    //Constructor
+    colour(String name, int r, int g, int b) {
+        this.commonName = name;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+
+    int[] getRGB(){
+        return new int[] {r,g,b};
+
+    }
+
+    String getHexCode(){
+        //Hex codes look like: #C805C8
+        String hexCode = "#"; // Starts as string with only the #
+
+        //Add r to the end as a hex string.
+        if(Integer.toHexString(r).length() < 2){
+            hexCode += "0";
+        }
+        hexCode += Integer.toHexString(r);
+
+        //Add g to the end as a hex string.
+        if(Integer.toHexString(g).length() < 2){
+            hexCode += "0";
+        }
+        hexCode += Integer.toHexString(g);
+
+        //Add b to the end as a hex string.
+        if(Integer.toHexString(b).length() < 2){
+            hexCode += "0";
+        }
+        hexCode += Integer.toHexString(b);
+
+        return  hexCode;
+    }
+
+    //This is an example of a more efficient implementation of the above. It is somewhat harder to understand.
+    //It uses an integer array and a foreach loop to remove redundant code.
+    String getHexCodeEfficiently(){
+        String hexCode = "#";
+        for (int c: getRGB()
+             ) {
+            if(Integer.toHexString(c).length() <2){
+                hexCode += "0";
+            }
+            hexCode += Integer.toHexString(c);
+        }
+
+        return hexCode;
+    }
+
+
+
+}
