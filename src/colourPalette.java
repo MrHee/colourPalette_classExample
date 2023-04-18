afd759fe093d35611c525ed1c536af72ebe25787
Mr Hee <matteo.hee@spschools.org>
2023-04-18 09:05:14 -0600

    Added colourPalette class and first constructor.

---
+++ src/colourPalette.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class colourPalette {
+
+
+    ArrayList<colour> colours;
+    colour mainColour;
+    int numberOfColours;
+
+    //Generative Constructors
+    //These will create colour palettes.
+
+    colourPalette(colour mainColour, int numberOfColours){
+        this.mainColour = mainColour;
+        this.numberOfColours = numberOfColours;
+        this.colours.add(mainColour);
+
+        //Second colour is complimentary.
+        if(numberOfColours > 1){
+            int newColourR = 255-mainColour.r;
+            int newColourG = 255-mainColour.g;
+            int newColourB = 255-mainColour.b;
+            colour temp = new colour(""+ mainColour.commonName +" 's compliment.",  newColourR, newColourG, newColourB);
+            colours.add(temp);
+        }
+
+    }
+
+
+    void display(){
+        for (colour c : colours
+             ) {
+            c.display();
+        }
+    }
+
+}
